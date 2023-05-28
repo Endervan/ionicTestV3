c6e72bf135319b161137c69062da7462372de6c8
edvan.ximenes <endvan@gmail.com>
2023-05-28 15:54:22 -0300

    test compare object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {DemoMaterialModule, MaterialModule} from "../shared/material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    BrowserAnimationsModule,
+    MaterialModule,
+    IonicModule.forRoot(MyApp),
+    DemoMaterialModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  form: FormGroup;
+  formInit = {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private fb: FormBuilder,
+    public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      textInput: [null],
+      textArea: [null],
+      inputSelect: [null],
+    })
+
+    this.formInit = this.form.value;
+  }
+
+
+  enviar() {
+    let edit = false;
+    edit = !this.compareObjects(this.formInit, this.form.value);
+
+    console.log(edit)
+  }
+
+
+  private compareObjects(init, after): boolean {
+    const k1 = Object.keys(init);
+    const k2 = Object.keys(after);
+
+    if (k1.length !== k2.length) return false;
+
+    for (let key of k1) {
+      if (after[key] == '') {
+        init[key] = null;
+        after[key] = null;
+      }
+      if ((init[key] !== after[key])) {
+        return false;
+      }
+    }
+    return true;
   }
 
 }
